6b8d8d220bb64c0de602739835e103d7a2e23974
Mark-A14 <marklacap14.ml@gmail.com>
2022-05-10 19:21:26 +0800

    Made buttons to functional

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ function playRound(playerSelection, computerSelection) {
      let outcome_win = `You Win! ${playerSelection} Beats ${computerSelection}!`;
      let outcome_lose = `You Lose! ${computerSelection} Beats ${playerSelection}!`;
 
-     if (playerSelection === computerSelection) {
+     if (playerSelection == computerSelection) {
           console.log("It's a tie");
           return;
-     } else if (playerSelection === "SCISSOR" && computerSelection === "PAPER"){
+     } else if (playerSelection == "SCISSOR" && computerSelection == "PAPER"){
           console.log(outcome_win);
           return true;
-     } else if (playerSelection === "ROCK" && computerSelection === "SCISSOR"){
+     } else if (playerSelection == "ROCK" && computerSelection == "SCISSOR"){
           console.log(outcome_win);
           return true;
-     } else if (playerSelection === "SCISSOR" && computerSelection === "PAPER"){
+     } else if (playerSelection == "SCISSOR" && computerSelection == "PAPER"){
           console.log(outcome_win);
           return true;
      } else {
@@ -26,28 +26,34 @@ function playRound(playerSelection, computerSelection) {
           return false;
      }
    }
-  
-function game() {
-     let player = 0;
-     let computer = 0
-     for (let i = 0; i < 5; i++){
-          let playerSelection = prompt('ROCK,PAPER or SCISSOR?')
-          let state = playRound(playerSelection.toUpperCase(), computerPlay());
-          if (state === true){
-               player++;
-          } 
-          if (state === false){
-               computer++;
-          }
-          if (state == undefined){
-               i--;
-          }
-     }
-     if (player > computer){
-          alert(`You Win! Your score: ${player} ||| Computer score: ${computer}`)
-     } else {
-          alert(`You Loss! Your score: ${player} ||| Computer score: ${computer}`)
-     }
-}
 
-game("ROCK");
+function test (){
+     playRound(this.id.toUpperCase(), computerPlay());
+}
+const playerPick = document.querySelectorAll('button');
+playerPick.forEach(play => play.addEventListener('click', test));
+   
+// function game() {
+//      let player = 0;
+//      let computer = 0
+//      for (let i = 0; i < 5; i++){
+//           let playerSelection = prompt('ROCK,PAPER or SCISSOR?')
+//           let state = playRound(playerSelection.toUpperCase(), computerPlay());
+//           if (state === true){
+//                player++;
+//           } 
+//           if (state === false){
+//                computer++;
+//           }
+//           if (state == undefined){
+//                i--;
+//           }
+//      }
+//      if (player > computer){
+//           alert(`You Win! Your score: ${player} ||| Computer score: ${computer}`)
+//      } else {
+//           alert(`You Loss! Your score: ${player} ||| Computer score: ${computer}`)
+//      }
+// }
+
+// game();
